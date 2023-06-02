da2515bc38f460b2715b8aae29c898cede1b032d
Johan Moreno <jomorenog@unal.edu.co>
2023-06-02 16:01:17 -0500

    Anado hadronico

---
+++ Hadr05/build/CMakeFiles/Hadr05.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/Hadr05.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/Hadr05.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/ActionInitialization.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/ActionInitialization.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/DetectorConstruction.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/DetectorConstruction.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/DetectorMessenger.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/DetectorMessenger.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/ElectromagneticPhysics.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/ElectromagneticPhysics.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/EventAction.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/EventAction.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/GammaNuclearPhysics.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/GammaNuclearPhysics.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/GammaNuclearPhysicsLEND.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/GammaNuclearPhysicsLEND.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/HadronElasticPhysicsHP.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/HadronElasticPhysicsHP.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/HistoManager.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/HistoManager.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/PhysicsList.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/PhysicsList.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/PrimaryGeneratorAction.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/RadioactiveDecayPhysics.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/RadioactiveDecayPhysics.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/Run.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/Run.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/RunAction.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/RunAction.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/SteppingAction.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/SteppingAction.cc.o"
+  "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/src/TrackingAction.cc" "/home/elsebas/software/geant4/geant4-v11.1.0/examples/extended/hadronic/Hadr05/build/CMakeFiles/Hadr05.dir/src/TrackingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "G4VIS_USE_TOOLSSG_QT_GLES"
+  "PTL_BUILD_DLL"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/elsebas/software/geant4/geant4-v11.1.0-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Hadr05/build/CMakeFiles/Hadr05.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hadr05.dir/Hadr05.cc.o"
+  "CMakeFiles/Hadr05.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/Hadr05.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/Hadr05.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/Hadr05.dir/src/ElectromagneticPhysics.cc.o"
+  "CMakeFiles/Hadr05.dir/src/EventAction.cc.o"
+  "CMakeFiles/Hadr05.dir/src/GammaNuclearPhysics.cc.o"
+  "CMakeFiles/Hadr05.dir/src/GammaNuclearPhysicsLEND.cc.o"
+  "CMakeFiles/Hadr05.dir/src/HadronElasticPhysicsHP.cc.o"
+  "CMakeFiles/Hadr05.dir/src/HistoManager.cc.o"
+  "CMakeFiles/Hadr05.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/Hadr05.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Hadr05.dir/src/RadioactiveDecayPhysics.cc.o"
+  "CMakeFiles/Hadr05.dir/src/Run.cc.o"
+  "CMakeFiles/Hadr05.dir/src/RunAction.cc.o"
+  "CMakeFiles/Hadr05.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/Hadr05.dir/src/TrackingAction.cc.o"
+  "Hadr05"
+  "Hadr05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hadr05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
