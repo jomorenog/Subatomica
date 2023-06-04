2e78ec83056427ec967914077e77d976b4c562fe
danjaro <dangarciahe@unal.edu.co>
2023-06-03 22:46:06 -0500

    Lo de Danitos

---
+++ B4b/build/CMakeFiles/exampleB4b.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/exampleB4b.cc" "CMakeFiles/exampleB4b.dir/exampleB4b.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/exampleB4b.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/ActionInitialization.cc" "CMakeFiles/exampleB4b.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/ActionInitialization.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/DetectorConstruction.cc" "CMakeFiles/exampleB4b.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/DetectorConstruction.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/EventAction.cc" "CMakeFiles/exampleB4b.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/EventAction.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB4b.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/RunAction.cc" "CMakeFiles/exampleB4b.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/RunAction.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/RunData.cc" "CMakeFiles/exampleB4b.dir/src/RunData.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/RunData.cc.o.d"
+  "/home/danjaro/Descargas/apps/geant4/geant4-v11.1.1/examples/basic/B4/B4b/src/SteppingAction.cc" "CMakeFiles/exampleB4b.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
