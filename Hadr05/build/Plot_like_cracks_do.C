0fd7de3fd2454e6be4f6ae630018de3a3bc3be5f
Johan Moreno <jomorenog@unal.edu.co>
2023-06-03 22:25:48 -0500

    Dimomedez el grande

--- Hadr05/build/Plot_like_cracks_do.C
+++ Hadr05/build/Plot_like_cracks_do.C
@@ -3,15 +3,38 @@
 
   // Dibujamos los histogramas como Jesus manda
   //Leemos el archivo como leemos la biblia
-  TFile f("W-lAr.root");
-
+  TFile fwlar("W-lAr.root");
+  TFile ffesci("Fe-Sci.root");
+  TFile fpblar("Pb-lAr.root");
+  
   //Creamos el visualizador, como visualizar a Dios en nuestro corazón
   TCanvas* c1 = new TCanvas("c1", " ");
   // Creamos el histograma, me gustaria crear una historia con ella
-  TH1D* h1 = (TH1D*)f.Get("21");
+  TH1D* h1 = (TH1D*)fwlar.Get("1");
+  TH1D* h2 = (TH1D*)ffesci.Get("1");
+  TH1D* h3 = (TH1D*)fpblar.Get("1");
+
+  
+  
+  h2->SetLineColor(kBlue);
+  h2->Draw("Hist");
+  c1->Update();
+
+  h3->SetLineColor(kGreen);
+  h3->Draw("same Hist");
+  
   h1->SetLineColor(kRed);
-  h1->Draw("Hist");
-  h1->SetTitle("Gay el que lo lea");
+  h1->Draw("same Hist");
+  //h1->SetTitle("Gay el que lo lea");
   c1->SetGrid();
   
+  TLegend* legend = new TLegend(0.13, 0.88, 0.45, 0.70);
+  legend->SetHeader("The Legend Title","C"); // option "C" allows to center the header
+  legend->AddEntry(h1,"W-lAr","l");
+  legend->AddEntry(h2,"Fe-Sci","l");
+  legend->AddEntry(h3,"Pb-lAr","l");
+  legend->SetFillColor(0);
+  legend->SetTextSize(0.04);
+  legend->SetTextFont(40);
+  legend->Draw();
 }
